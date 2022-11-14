425fc262ac6ac667e440153c69260c3c1d97b9dc
cryptophrank <107065606+cryptophrank@users.noreply.github.com>
2022-11-14 05:47:16 -0800

    Added one line for Tarzan

--- abe-try2/pages/index.js
+++ abe-try2/pages/index.js
@@ -27,6 +27,7 @@ const allowlistedDiscordIds = ["855616810525917215", // TheLunaLabs
                                "147733347009953792", // cryptogorilla; added 2022-11-11 14:00 PST
                                "586537611464540161", // safz alpha gems; added 2022-11-11 14:00 PST
                                "553181792102907935", // frosty the gym club; added 2022-11-13 20:45 PST
+                               "755510557452402796", // added asked by tarzan; added 2022-11-14 20:45 PST
                                "732387893196226660", // obi-wan dinobi; added 2022-11-13 17:48 CET
                                //"904305352990933003", // JB
                               ] // main, JB
