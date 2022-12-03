06061325eb5ec1fc4fd2321400f648251ccf1e16
cryptophrank <107065606+cryptophrank@users.noreply.github.com>
2022-12-02 19:07:25 -0800

    dded LiamdNFT

--- abe-try2/pages/index.js
+++ abe-try2/pages/index.js
@@ -36,6 +36,7 @@ const allowlistedDiscordIds = ["855616810525917215", // TheLunaLabs
                                "563729224352006163", // from cryptophrank lordmarlborough; 2022-11-18 6:30a PST
                                "280799503454306304", // from cryptophrank natentate; 2022-11-18 6:30a PST
                                "696648129658880020", // aray.eth (alpha gems); 2022-12-01 00:25a CET
+                               "894403880442544158", // LiamDnft friend of NFTNate's; 2022-12-02 19:00 PST
                                //"904305352990933003", // JB
                               ] // main, JB
 
